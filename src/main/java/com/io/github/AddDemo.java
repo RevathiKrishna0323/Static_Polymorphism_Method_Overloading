036edba6b3ca66e91478ca969d8f312bdcd25af7
Revathi G <revathikrishna0323@gmail.com>
2022-10-13 17:58:24 +0530

    added methods inside the AddDemo class

--- src/main/java/com/io/github/AddDemo.java
+++ src/main/java/com/io/github/AddDemo.java
@@ -6,5 +6,25 @@
 
 package com.io.github;
 
-public class AddDemo {
-}
+    public class AddDemo {
+
+        public void add(int num1, int num2, int num3) {
+            System.out.println(num1+num2 + num3);
+        }
+        public void add(float num1, float num2) {
+            System.out.println(num1+num2);
+        }
+        public void add(int num1, int num2) {
+            System.out.println(num1+num2);
+
+        }
+        public static void main(String[] args) {
+            AddDemo addDemo = new AddDemo();
+            addDemo.add(10, 12);
+            addDemo.add(12, 23, 34);
+            addDemo.add(12.5f, 4.5f);
+
+        }
+
+
+    }
